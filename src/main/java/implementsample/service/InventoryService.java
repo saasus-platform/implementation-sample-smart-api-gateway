f1d1f8aebb29ec8b0c5896406e691aa64d6b1b62
Akira Yasuzawa <Akira.yasuzawa@anti-pattern.co.jp>
2025-07-01 10:49:17 +0900

    remove unused controller class

--- src/main/java/implementsample/service/InventoryService.java
+++ src/main/java/implementsample/service/InventoryService.java
@@ -8,24 +8,27 @@ import saasus.sdk.modules.SaaSusAPI;
 import saasus.sdk.util.apiserver.SaaSusIdentity;
 
 public class InventoryService {
-    public List<InventoryDto> getInventory(String tenantId) {
-        // テナントIDに対応する在庫情報取得
-        return InventoryRepository.TenantInventory.getOrDefault(tenantId,
-                InventoryRepository.TenantInventory.get("default"));
-    }
-    
-    // Step 2: ソースコードへのアノテーション設定
+
+    /**
+     * 在庫情報を取得するAPI
+     * @param identity SaaSusIdentity オブジェクト
+     * @return 在庫情報のリスト
+     */
     @SaaSusAPI(path = "getInventory")
-    public static List<InventoryDto> getInventoryEntryPoint(SaaSusIdentity identity) {
+    public static List<InventoryDto> getInventory(SaaSusIdentity identity) {
         String tenantId = identity.getTenantId();
         String envId = identity.getEnvId();
-         
+
         return InventoryRepository.getInventory(tenantId, envId);
     }
 
+    /**
+     * ユーザごとの在庫情報を取得するAPI
+     * @param identity SaaSusIdentity オブジェクト
+     * @return 在庫情報のリスト
+     */
     @SaaSusAPI(path = "getInventoryByUser")
     public static List<InventoryDto> getInventoryByUser(SaaSusIdentity identity) {
-        // ユーザIDを取得
         String tenantId = identity.getTenantId();
         String envId = identity.getEnvId();
         String userId = identity.getUserId();
